3f6389834fae5c80727e49360baca4f8080f5caa
med_aboub <m.gaddour1981@gmail.com>
2024-09-29 08:50:52 +0000

    add l label

--- section2/install.cmd
+++ section2/install.cmd
@@ -3,7 +3,7 @@ helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/
 #Install Helm Chart for NFS
 helm install nfs-subdir-external-provisioner \
 nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
---set nfs.server=192.168.60.5 \
+--set nfs.server=192.168.60.50 \
 --set nfs.path=/data \
 --set storageClass.onDelete=true
 # Check pods and storage classes:
