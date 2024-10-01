3b0292b58c7b5da5de337db1100ff18ceee462cd
gaddour1981med <114749103+gaddour1981med@users.noreply.github.com>
2024-10-01 16:52:18 +0200

    Update nfs_client.sh

--- section1/k8s/scripts/nfs_client.sh
+++ section1/k8s/scripts/nfs_client.sh
@@ -7,6 +7,6 @@ mkdir /mnt/data
 grep "192.168.60.5:/data" /etc/fstab
 
 if [ $? -ne 0 ] ; then
-    echo "192.168.60.5:/data /mnt/data nfs defaults 0 0" >> /etc/fstab
+    echo "192.168.60.50:/data /mnt/data nfs defaults 0 0" >> /etc/fstab
     mount -a
 fi
