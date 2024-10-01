1177eebf32c74e5f58829feb1b8232bec5b251bd
gaddour1981med <114749103+gaddour1981med@users.noreply.github.com>
2024-10-01 22:17:18 +0200

    Update install.cmd

--- section1/dashboard/install.cmd
+++ section1/dashboard/install.cmd
@@ -3,4 +3,4 @@ kubectl get pods -A
 kubectl apply -f dashboard-admin.yaml
 kubectl patch svc kubernetes-dashboard --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"}]'  -n kubernetes-dashboard
 #get dashboard admin token
-kubectl get secret -n kubernetes-dashboard $(kubectl get serviceaccount admin-user -n kubernetes-dashboard -o jsonpath="{.secrets[0].name}") -o jsonpath="{.data.token}" | base64 --decode
+kubectl get secret admin-user -n kubernetes-dashboard -o jsonpath={".data.token"} | base64 -d
