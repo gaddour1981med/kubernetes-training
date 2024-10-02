19d8e30f63d1a5ae18ea9a5d6e321cd6d02a3a8a
medaboub <m.gaddour@yahoo.fr>
2024-10-02 22:12:58 +0200

    adding dnsmask

--- section4/install.cmd
+++ section4/install.cmd
@@ -5,3 +5,11 @@ export PATH=$PWD/bin:$PATH
 istioctl install 
 #add istio injection to default namespace
 kubectl label namespace default istio-injection=enabled
+
+
+#install dnsmask
+sudo systemctl disable --now systemd-resolved
+sudo systemctl stop systemd-resolved
+sudo apt install dnsmasq
+#ajouter le fichier config de dnsmask
+sudo systemctl restart dnsmasq
