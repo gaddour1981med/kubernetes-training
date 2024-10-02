16d9c6415539b98898112da2a56eaa0d438ae6b6
medaboub <m.gaddour@yahoo.fr>
2024-10-02 11:57:30 +0200

    add section 3 metallb

---
+++ section3/install.cmd
@@ -0,0 +1,23 @@
+#install metallb
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
+
+#config metallb pool
+cat <<EOF | kubectl apply -n metallb-system -f -
+apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
+metadata:
+  name: first-pool
+  namespace: metallb-system
+spec:
+  addresses:
+  - 192.168.60.50-192.168.60.60
+---
+apiVersion: metallb.io/v1beta1
+kind: L2Advertisement
+metadata:
+  name: example
+  namespace: metallb-system
+spec:
+  ipAddressPools:
+  - first-pool
+EOF
